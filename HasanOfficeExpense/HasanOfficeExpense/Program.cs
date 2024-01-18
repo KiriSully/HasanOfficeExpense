afc1b94901830f1ee8e055890c85802599b29b69
KiriSully <122448179+KiriSully@users.noreply.github.com>
2024-01-18 23:21:37 +0200

    Add files via upload

--- HasanOfficeExpense/HasanOfficeExpense/Program.cs
+++ HasanOfficeExpense/HasanOfficeExpense/Program.cs
@@ -14,7 +14,7 @@ namespace HasanOfficeExpense
         private static readonly string userPath = @"Data\Users\";
         private static readonly string reportsFolderPath = @"Data\Reports\";
         private static string user = "Guest";
-        private static List<string> availableCategories = new List<string> { "Офісні матеріали", "Послуги", "Їжа та напої", "Транспорт" };
+        public static List<string> availableCategories = new List<string> { "Офісні матеріали", "Послуги", "Їжа та напої", "Транспорт" };
         private static int quantityUsers = 0;
         private static RegistrationManager registrationManager = new RegistrationManager(userPath);
         public static string GetDailyExpensesFilePath()
@@ -305,7 +305,6 @@ namespace HasanOfficeExpense
             }
         }
 
-
         public static void Mainmenu()
         {
             Console.Clear();
@@ -441,13 +440,13 @@ namespace HasanOfficeExpense
             switch (key)
             {
                 case '1':
-                    SearchByDate();
+                    ExpenseSearchManager.SearchByDate(expenses, availableCategories);
                     break;
                 case '2':
-                    SearchByCategory();
+                    ExpenseSearchManager.SearchByCategory(expenses, availableCategories);
                     break;
                 case '3':
-                    SearchByDescription();
+                    ExpenseSearchManager.SearchByDescription(expenses, availableCategories);
                     break;
                 case '0':
                     UserMainMenu.UserFunctionality();
@@ -456,84 +455,6 @@ namespace HasanOfficeExpense
                     ChooseSearchCriteria();
                     break;
             }
-        }
-       
-        private static void SearchByDate()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "User");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│       Пошук витрат за датою     │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.Write("Введіть дату (рррр-мм-дд): ");
-            string inputDate = Console.ReadLine();
-
-            if (DateTime.TryParseExact(inputDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime searchDate))
-            {
-                var searchResults = expenses.Where(e => e.Date.Date == searchDate.Date).ToList();
-
-                DisplaySearchResults(searchResults);
-            }
-            else
-            {
-                Console.WriteLine("Невірний формат дати.");
-            }
-
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteria();
-        }
-        private static void SearchByCategory()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "User");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│    Пошук витрат за категорією    │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.WriteLine("Доступні категорії:");
-
-            for (int i = 0; i < availableCategories.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {availableCategories[i]}");
-            }
-
-            Console.Write("Виберіть номер категорії: ");
-            int categoryChoice;
-
-            while (!int.TryParse(Console.ReadLine(), out categoryChoice) || categoryChoice < 1 || categoryChoice > availableCategories.Count)
-            {
-                Console.WriteLine("Невірний вибір. Будь ласка, введіть коректний номер категорії.");
-                Console.Write("Виберіть номер категорії: ");
-            }
-
-            string selectedCategory = availableCategories[categoryChoice - 1];
-            var searchResults = expenses.Where(e => string.Equals((string)e.Category, selectedCategory, StringComparison.OrdinalIgnoreCase))
-                                        .ToList();
-            DisplaySearchResults(searchResults);
-
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteria();
-        }
-        private static void SearchByDescription()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "User");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│   Пошук витрат за описом витрати   │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.Write("Введіть опис витрати: ");
-            string searchDescription = Console.ReadLine();
-            var searchResults = expenses
-                .Where(e => e.Description is string description && description.IndexOf(searchDescription, StringComparison.OrdinalIgnoreCase) >= 0)
-                .ToList();
-            DisplaySearchResults(searchResults);
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteria();
         }
         public static void ChooseSearchCriteriaAdmin()
         {
@@ -559,13 +480,13 @@ namespace HasanOfficeExpense
             switch (key)
             {
                 case '1':
-                    SearchByDateAdmin();
+                    ExpenseSearchManager.SearchByDateAdmin(expenses, availableCategories);
                     break;
                 case '2':
-                    SearchByCategoryAdmin();
+                    ExpenseSearchManager.SearchByCategoryAdmin(expenses, availableCategories);
                     break;
                 case '3':
-                    SearchByDescriptionAdmin();
+                    ExpenseSearchManager.SearchByDescriptionAdmin(expenses, availableCategories);
                     break;
                 case '0':
                     UserMainMenu.AdminFunctionality();
@@ -596,7 +517,6 @@ namespace HasanOfficeExpense
             Console.ReadKey();
             UserMainMenu.UserFunctionality();
         }
-
         private static void DisplaySearchResultsAdmin(List<ClassExpense.Expense> matchingExpenses)
         {
             Console.Clear();
@@ -617,87 +537,6 @@ namespace HasanOfficeExpense
             Console.WriteLine("Натисніть будь-яку клавішу для повернення в меню адміністратора.");
             Console.ReadKey();
             UserMainMenu.AdminFunctionality();
-        }
-
-        private static void SearchByDateAdmin()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "Admin");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│       Пошук витрат за датою     │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.Write("Введіть дату (рррр-мм-дд): ");
-            string inputDate = Console.ReadLine();
-
-            if (DateTime.TryParseExact(inputDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime searchDate))
-            {
-                var searchResults = expenses.Where(e => e.Date.Date == searchDate.Date).ToList();
-
-                DisplaySearchResultsAdmin(searchResults);
-            }
-            else
-            {
-                Console.WriteLine("Невірний формат дати.");
-            }
-
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteriaAdmin();
-        }
-
-        private static void SearchByCategoryAdmin()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "Admin");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│    Пошук витрат за категорією    │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│    Пошук витрат за категорією    │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.WriteLine("Доступні категорії:");
-
-            for (int i = 0; i < availableCategories.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {availableCategories[i]}");
-            }
-
-            Console.Write("Виберіть номер категорії: ");
-            int categoryChoice;
-
-            while (!int.TryParse(Console.ReadLine(), out categoryChoice) || categoryChoice < 1 || categoryChoice > availableCategories.Count)
-            {
-                Console.WriteLine("Невірний вибір. Будь ласка, введіть коректний номер категорії.");
-                Console.Write("Виберіть номер категорії: ");
-            }
-
-            string selectedCategory = availableCategories[categoryChoice - 1];
-            var searchResults = expenses.Where(e => ((string)e.Category).Equals(selectedCategory, StringComparison.OrdinalIgnoreCase)).ToList();
-            DisplaySearchResults(searchResults);
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteriaAdmin();
-        }
-        private static void SearchByDescriptionAdmin()
-        {
-            Console.Clear();
-            UserAuthentication.WriteUserRole(user, "Admin");
-            Console.WriteLine("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
-            Console.WriteLine("│   Пошук витрат за описом витрати   │");
-            Console.WriteLine("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
-
-            Console.Write("Введіть опис витрати: ");
-            string searchDescription = Console.ReadLine();
-            var searchResults = expenses
-                .Where(e => e.Description is string description && description.IndexOf(searchDescription, StringComparison.OrdinalIgnoreCase) >= 0)
-                .ToList();
-            DisplaySearchResultsAdmin(searchResults);
-            Console.WriteLine("Натисніть будь-яку клавішу для продовження.");
-            Console.ReadKey();
-            ChooseSearchCriteriaAdmin();
         }
     }
 }
